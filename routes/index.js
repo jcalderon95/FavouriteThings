d11abf310296db5edf990738346ad1ca0d2c2934
Jimmy Calderon <JimmyCalderon@Jimmys-MacBook-Pro.local>
2019-11-03 02:56:43 -0500

    added images and database data

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,16 @@ router.get('/', (req, res) => {
     // should really get the user data here and then fetch it thru, but let's try this asynchronously
     console.log('at the main route');
 
-    
-    res.render('home');
+    let query = "SELECT ID, Name, Image FROM tbl_items";
+
+    sql.query(query, (err, result) => {
+        if (err) { throw err; console.log(err); }
+
+        // console.log(result); 
+
+        // render the home view with dynamic data
+        res.render('home' , { item: result });
+    })
     
 })
 
